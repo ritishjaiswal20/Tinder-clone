0fe76b64a8ae77cd76d2810433294e16df42acd9
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-08 16:57:04 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>lets build tinder</h1>
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am a chat page</h1>
+          </Route>
+
+          <Route path="/">
+            // default route should always in bottom
+            <h1>I am a home page</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
